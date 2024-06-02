dfe64a8c1a39126fbd778d35260ae17e4081295b
keisuke-kamijo <keisuke.kamijo@supership.jp>
2024-06-02 19:19:06 +0900

    [#10] 既存のUserモデルを削除

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_25_105506) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
