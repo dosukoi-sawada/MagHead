556519fa72447dab831b05aabd213e9b57d7dc12
keisuke-kamijo <keisuke.kamijo@supership.jp>
2024-06-02 18:33:49 +0900

    add test api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
+    end
+  end
   get 'login', to: 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
